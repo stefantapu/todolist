7f9ad5b9afa3cb7730a5597ce01d4869a54a11a8
Stefan <stefantapu@gmail.com>
2025-10-06 12:57:43 +0200

    Configured App Architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -32,12 +32,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
