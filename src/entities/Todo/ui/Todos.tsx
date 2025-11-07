256ec1ea552c464602312155368e6610dec221f7
Stefan <stefantapu@gmail.com>
2025-11-07 10:19:19 +0100

    Enhance Todos component: add button for adding new tasks and improve setTodo functionality

--- src/entities/Todo/ui/Todos.tsx
+++ src/entities/Todo/ui/Todos.tsx
@@ -1,8 +1,9 @@
 import Box from '@mui/material/Box';
-import { Grid, Input } from '@mui/material';
+import { Button, Grid, Input } from '@mui/material';
 import { useTodosStore } from '../model/store/useTodosStore';
 import { Todo } from './Todo';
 import { useState } from 'react';
+import type { TodoType } from '../model/todoType';
 
 //  Корневой компонент списка задач
 const Todos = () => {
@@ -11,18 +12,20 @@ const Todos = () => {
   const [newTodoDescription, setNewTodoDescription] = useState('');
 
   const todos = useTodosStore(state => state.todos);
+  const addTodos = useTodosStore(state => state.addTodo);
+  const setTodos = useTodosStore(state => state.setTodos);
 
-  //   // Функция для обновления одной задачи в массиве:
-  //   // проходит по всем задачам и заменяет ту, у которой совпадает _id
-  //   const setTodo = (todo: TodoType) => {
-  //     const updateTodos = todos.map((t: TodoType) => {
-  //       if (t._id === todo._id) {
-  //         return todo; // возвращаем обновлённую задачу
-  //       }
-  //       return t; // остальные оставляем без изменений
-  //     });
-  //     setTodos(updateTodos); // обновляем стейт
-  //   };
+  // Функция для обновления одной задачи в массиве:
+  // проходит по всем задачам и заменяет ту, у которой совпадает _id
+  const setTodo = (todo: TodoType) => {
+    const updatedTodos = todos.map((t: TodoType) => {
+      if (t._id === todo._id) {
+        return todo; // возвращаем обновлённую задачу
+      }
+      return t; // остальные оставляем без изменений
+    });
+    setTodos(updatedTodos); // обновляем стейт
+  };
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewTodoTitle(e.target.value);
@@ -31,6 +34,18 @@ const Todos = () => {
     setNewTodoDescription(e.target.value);
   };
 
+  const handldeAddTodo = () => {
+    const newTodo: TodoType = {
+      _id: Date.now().toString(),
+      title: newTodoTitle,
+      description: newTodoDescription,
+      completed: false,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      order: todos.length + 1,
+    };
+    addTodos(newTodo);
+  };
   return (
     // Контейнер для сетки карточек
     <Box sx={{ flexGrow: 1 }}>
@@ -44,7 +59,9 @@ const Todos = () => {
         value={newTodoDescription}
         onChange={handleDescriptionChange}
       />
-
+      <Button disabled={!newTodoTitle} onClick={handldeAddTodo}>
+        Add
+      </Button>
       <Grid
         container
         direction="row"
@@ -57,7 +74,7 @@ const Todos = () => {
       >
         {/* Рендерим каждую задачу через компонент Todo */}
         {todos.map(todo => {
-          return <Todo todo={todo} key={todo._id} />;
+          return <Todo todo={todo} key={todo._id} setTodo={setTodo} />;
         })}
       </Grid>
     </Box>
